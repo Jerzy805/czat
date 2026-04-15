375100672b39f1495d55fc5bd7801175fda1cb7c
Jerzy805 <140313409+Jerzy805@users.noreply.github.com>
2026-04-15 21:21:12 +0200

    zmiana kursora i nazywania plików

--- chat.c
+++ chat.c
@@ -7,80 +7,70 @@
 
 #define MAX 256
 
-char my_file[100];
-char other_file[100];
+char my_file[150];
+char other_file[150];
 char my_name[50];
 
-// Wątek do czytania wiadomości od TEGO DRUGIEGO (tylko odczyt)
-void *reader(void *arg)
-{
+void *reader(void *arg) {
     FILE *f;
     char line[MAX];
 
-    // Czekaj na plik kolegi
-    while ((f = fopen(other_file, "r")) == NULL)
-    {
-        usleep(500000); 
+    // Czekaj na plik kolegi (np. chat_emil-jerzy)
+    while ((f = fopen(other_file, "r")) == NULL) {
+        usleep(500000);
     }
 
-    // Czytaj istniejące i nowe
-    while (1)
-    {
-        if (fgets(line, MAX, f) != NULL)
-        {
-            printf("%s", line);
+    // Przeskocz do końca, żeby nie czytać historii przy wejściu
+    fseek(f, 0, SEEK_END);
+
+    while (1) {
+        if (fgets(line, MAX, f) != NULL) {
+            printf("\r\033[K%s> ", line); // Wyświetl wiadomość i przywróć znak zachęty
             fflush(stdout);
-        }
-        else
-        {
+        } else {
             clearerr(f);
-            usleep(200000);
+            usleep(100000);
         }
     }
     return NULL;
 }
 
-int main(int argc, char *argv[])
-{
-    if (argc != 3)
-    {
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
         printf("Użycie: %s <twój_nick> <nick_kolegi>\n", argv[0]);
         return 1;
     }
 
     strcpy(my_name, argv[1]);
 
-    // Definiujemy pliki:
-    // MÓJ plik (do którego JA piszę)
-    snprintf(my_file, sizeof(my_file), "/tmp/msg_%s", argv[1]);
-    // PLIK KOLEGI (z którego JA tylko czytam)
-    snprintf(other_file, sizeof(other_file), "/tmp/msg_%s", argv[2]);
+    // Format: chat_JA-KOLEGA (to Twój plik do zapisu)
+    snprintf(my_file, sizeof(my_file), "/tmp/chat_%s-%s", argv[1], argv[2]);
+    // Format: chat_KOLEGA-JA (to plik kolegi, który czytasz)
+    snprintf(other_file, sizeof(other_file), "/tmp/chat_%s-%s", argv[2], argv[1]);
 
-    // Przed startem upewnijmy się, że mój plik ma uprawnienia do czytania dla kolegi
+    // Utwórz swój plik i daj innym prawo do czytania (0644)
     FILE *init = fopen(my_file, "a");
     if (init) {
         fclose(init);
-        chmod(my_file, 0644); // Właściciel pisze, inni tylko czytają
+        chmod(my_file, 0644);
     }
+
+    printf("Rozpoczęto czat z %s\n", argv[2]);
+    printf("Twój plik: %s\n", my_file);
+    printf("Czytam z:  %s\n", other_file);
 
     pthread_t tid;
     pthread_create(&tid, NULL, reader, NULL);
 
     char msg[MAX];
-    while (1)
-    {
-        if (fgets(msg, MAX, stdin) != NULL)
-        {
-            // Otwieramy TYLKO SWÓJ plik (do którego mamy prawo własności)
+    while (1) {
+        printf("> ");
+        fflush(stdout);
+        if (fgets(msg, MAX, stdin) != NULL) {
             FILE *f = fopen(my_file, "a");
-            if (f != NULL)
-            {
+            if (f != NULL) {
                 fprintf(f, "[%s] %s", my_name, msg);
                 fclose(f);
-            }
-            else
-            {
-                perror("Błąd zapisu do Twojego pliku");
             }
         }
     }
