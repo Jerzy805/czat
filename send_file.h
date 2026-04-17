95e758355d2f2d94b3ac2f2c4484d283e7e78af1
Jerzy805 <j.chmiel2006@gmail.com>
2026-04-17 12:24:54 +0200

    dodanie send_file .c i .h

---
+++ send_file.h
@@ -0,0 +1,6 @@
+#ifndef SEND_FILE_H
+#define SEND_FILE_H
+
+void send_file(const char *my_file, const char *filename, char id[30]); // pierwszy argument to plik, w którym zapisywane są własne wiadomości
+
+#endif
