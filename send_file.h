01e6088e75fb3d1e3eb0f218596cf2fc98e9e42a
Jerzy805 <140313409+Jerzy805@users.noreply.github.com>
2026-04-24 09:48:19 +0200

    Delete send_file.h

--- send_file.h
+++
@@ -1,6 +0,0 @@
-#ifndef SEND_FILE_H
-#define SEND_FILE_H
-
-void send_file(const char *my_file, const char *filename, char id[30]); // pierwszy argument to plik, w którym zapisywane są własne wiadomości
-// w obecnej postaci funkcja kopiuje plik do folderu /tmp/ i nadaje prawo czytania rozmówcy
-#endif
